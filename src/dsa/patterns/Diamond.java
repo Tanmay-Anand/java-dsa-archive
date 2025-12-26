815b14e6b5c23115d5030f2a2a1965483882cf63
Tanmay-Anand <tanmayanand2002@gmail.com>
2025-12-26 22:50:42 +0530

    Star pattern questions

--- src/dsa/patterns/Diamond.java
+++ src/dsa/patterns/Diamond.java
@@ -1,4 +1,42 @@
 package dsa.patterns;
 
 public class Diamond {
+
+    public void meth() {
+
+        int n = 5;
+
+        // upper half
+        for (int i = 0; i < n; i++) {
+
+            for (int s = 0; s < n - i - 1; s++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        // lower half
+        for (int i = n - 2; i >= 0; i--) {
+
+            for (int s = 0; s < n - i - 1; s++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Diamond di = new Diamond();
+        di.meth();
+    }
 }
